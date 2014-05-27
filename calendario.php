bcfba2bd6981f449474aaed52e3e48a9a1063068
jorgetec89 <new_shady89@hotmail.com>
2014-05-27 10:48:57 -0500

    Create calendario.php

---
+++ calendario.php
@@ -0,0 +1,245 @@
+
+
+<body>
+<head>
+<link type="text/css" rel="stylesheet" href="css/calendario.css">
+
+</head>
+<body>
+<div class="dp_pec_wrapper dp_pec_monthly light" id="dp_pec_id872955989" style="width: 100% ">
+				
+				<div class="dp_pec_nav dp_pec_nav_monthly">
+					<span class="prev_month">« <strong>Prev Month</strong></span>
+					<span class="actual_month">January 2014</span>
+					<span class="next_month"><strong>Next Month</strong> »</span>
+				</div>
+			
+				<div class="dp_pec_nav dp_pec_nav_daily" style="display:none;">
+					<span class="prev_day">« <strong>Prev Day</strong></span>
+					<span class="actual_day">January 28, 2014</span>
+					<span class="next_day"><strong>Next Day</strong> »</span>
+				</div>
+					
+				<div style="clear:both;"></div>
+				
+				<div class="dp_pec_content isDraggable">
+					
+			     <div class="dp_pec_dayname">
+						<span>Sunday</span>
+				 </div>
+				 <div class="dp_pec_dayname">
+						<span>Monday</span>
+				 </div>
+				 <div class="dp_pec_dayname">
+						<span>Tuesday</span>
+				 </div>
+				 <div class="dp_pec_dayname">
+						<span>Wednesday</span>
+				 </div>
+				 <div class="dp_pec_dayname">
+						<span>Thursday</span>
+				 </div>
+				 <div class="dp_pec_dayname">
+						<span>Friday</span>
+				 </div>
+				 <div class="dp_pec_dayname">
+						<span>Saturday</span>
+				 </div>
+				 
+					<div class="dp_pec_date disabled first-child">
+						<div class="dp_date_head"><span>29</span></div>
+					</div>
+					<div class="dp_pec_date disabled ">
+						<div class="dp_date_head"><span>30</span></div>
+					</div>
+					<div class="dp_pec_date disabled ">
+						<div class="dp_date_head"><span>31</span></div>
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-01">
+						<div class="dp_date_head"><span>1</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-02">
+						<div class="dp_date_head"><span>2</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-03">
+						<div class="dp_date_head"><span>3</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-04">
+						<div class="dp_date_head"><span>4</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date first-child" data-dppec-date="2014-01-05">
+						<div class="dp_date_head"><span>5</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-06">
+						<div class="dp_date_head"><span>6</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-07">
+						<div class="dp_date_head"><span>7</span></div>
+						<span class="dp_count_events">1</span>
+						
+						<div class="eventsPreview">
+							<ul>
+						<li><span>10:00</span>Weekly Event<div class="dp_pec_clear"></div><img width="300" height="212" src="http://wpsleek.com/wp-content/uploads/2013/12/photodune-2898652-scenic-route-through-the-mountains-m-300x212.jpg" class="attachment-medium wp-post-image" alt="Scenic Route Through the Mountains"></li>
+							</ul>
+						</div>
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-08">
+						<div class="dp_date_head"><span>8</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-09">
+						<div class="dp_date_head"><span>9</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-10">
+						<div class="dp_date_head"><span>10</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-11">
+						<div class="dp_date_head"><span>11</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date first-child" data-dppec-date="2014-01-12">
+						<div class="dp_date_head"><span>12</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-13">
+						<div class="dp_date_head"><span>13</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-14">
+						<div class="dp_date_head"><span>14</span></div>
+						<span class="dp_count_events">1</span>
+						
+						<div class="eventsPreview">
+							<ul>
+						<li><span>10:00</span>Weekly Event<div class="dp_pec_clear"></div><img width="300" height="212" src="http://wpsleek.com/wp-content/uploads/2013/12/photodune-2898652-scenic-route-through-the-mountains-m-300x212.jpg" class="attachment-medium wp-post-image" alt="Scenic Route Through the Mountains"></li>
+							</ul>
+						</div>
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-15">
+						<div class="dp_date_head"><span>15</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-16">
+						<div class="dp_date_head"><span>16</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-17">
+						<div class="dp_date_head"><span>17</span></div>
+						<span class="dp_count_events">1</span>
+						
+						<div class="eventsPreview">
+							<ul>
+						<li><span>10:00</span>Monthly Event<div class="dp_pec_clear"></div><img width="300" height="94" src="http://wpsleek.com/wp-content/uploads/2013/12/photodune-2898668-gears-of-industry-m-e1386700225507-300x94.jpg" class="attachment-medium wp-post-image" alt="Gears of Industry"></li>
+							</ul>
+						</div>
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-18">
+						<div class="dp_date_head"><span>18</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date first-child" data-dppec-date="2014-01-19">
+						<div class="dp_date_head"><span>19</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-20">
+						<div class="dp_date_head"><span>20</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-21">
+						<div class="dp_date_head"><span>21</span></div>
+						<span class="dp_count_events">1</span>
+						
+						<div class="eventsPreview">
+							<ul>
+						<li><span>10:00</span>Weekly Event<div class="dp_pec_clear"></div><img width="300" height="212" src="http://wpsleek.com/wp-content/uploads/2013/12/photodune-2898652-scenic-route-through-the-mountains-m-300x212.jpg" class="attachment-medium wp-post-image" alt="Scenic Route Through the Mountains"></li>
+							</ul>
+						</div>
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-22">
+						<div class="dp_date_head"><span>22</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-23">
+						<div class="dp_date_head"><span>23</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-24">
+						<div class="dp_date_head"><span>24</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-25">
+						<div class="dp_date_head"><span>25</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date first-child" data-dppec-date="2014-01-26">
+						<div class="dp_date_head"><span>26</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-27">
+						<div class="dp_date_head"><span>27</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-28" style="background-color: #81abeb;">
+						<div class="dp_date_head"><span>28</span></div>
+						<span class="dp_count_events">1</span>
+						
+						<div class="eventsPreview">
+							<ul>
+						<li><span>10:00</span>Weekly Event<div class="dp_pec_clear"></div><img width="300" height="212" src="http://wpsleek.com/wp-content/uploads/2013/12/photodune-2898652-scenic-route-through-the-mountains-m-300x212.jpg" class="attachment-medium wp-post-image" alt="Scenic Route Through the Mountains"></li>
+							</ul>
+						</div>
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-29">
+						<div class="dp_date_head"><span>29</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-30">
+						<div class="dp_date_head"><span>30</span></div>
+						
+						
+					</div>
+					<div class="dp_pec_date " data-dppec-date="2014-01-31">
+						<div class="dp_date_head"><span>31</span></div>
+						
+						
+					</div>
+						<div class="dp_pec_date disabled ">
+							<div class="dp_date_head"><span>1</span></div>
+						</div><div class="clear"></div>
+								
+				</div>
+			</div>
+</body>
+</html>
